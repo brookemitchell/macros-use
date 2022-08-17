d3a41d4eec3261ece7030291808e540fc24dc885
Brooke Mitchell <927600+brookemitchell@users.noreply.github.com>
2022-08-17 21:49:57 +1200

    add tracer macro

--- lib/tracer.ex
+++ lib/tracer.ex
@@ -1,2 +1,20 @@
 defmodule Tracer do
+  def dump_args(args) do
+    args |> Enum.map(&inspect/1) |> Enum.join(", ")
+  end
+
+  def dump_defn(name, args) do
+    "#{name}(#{dump_args(args)})"
+  end
+
+  defmacro def({name, _, args} = definition, do: content) do
+    quote do
+      Kernel.def unquote(definition) do
+        IO.puts("==> call    #{Tracer.dump_defn(unquote(name), unquote(args))}")
+        result = unquote(content)
+        IO.puts("<== result: #{result}")
+        result
+      end
+    end
+  end
 end
--- test/tracer_test.exs
+++ test/tracer_test.exs
@@ -1,5 +1,8 @@
 defmodule TracerTest do
   use ExUnit.Case
+  import Kernel, except: [def: 2]
+  import Tracer, only: [def: 2]
+
   import ExUnit.CaptureIO
 
   def puts_sum_3(a, b, c), do: a + b + c
@@ -8,12 +11,12 @@ defmodule TracerTest do
   test "puts sum 3" do
     assert with_io(fn ->
              puts_sum_3(1, 2, 3)
-           end) == {"==> call    puts_sum_3(1,2,3)", 6, "<== returns 6"}
+           end) == {6, "==> call    puts_sum_3(1, 2, 3)\n<== result: 6\n"}
   end
 
   test "add list" do
     assert with_io(fn ->
              add_list([1, 2, 3])
-           end) == {"==> call    add_list([1,2,3])", 6, "<== returns 6"}
+           end) == {6, "==> call    add_list([1, 2, 3])\n<== result: 6\n"}
   end
 end
